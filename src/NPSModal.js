8f6f15c3b2543daee9854b73d96d00db043e02c6
Hayley Berkebile <hayley@storyblocks.com>
2018-01-30 14:12:14 -0500

    Starting basic project set up. Outlining react component.

---
+++ src/NPSModal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class NPSModal extends React.Component {
+
+    static propTypes = {
+        mainQuestion: React.PropTypes.string.isRequired
+    }
+
+    constructor(props) {
+        super(props);
+    }
+
+}
+
+export default NPSModal
---
+++ src/NPSScale.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class NPSScale extends React.Component {
+
+}
+
+export default NPSScale
