23648362637e71b144c025ea44e50f58ae2350e7
Hayley Berkebile <hayley@storyblocks.com>
2018-01-31 14:01:14 -0500

    Finishing up most of render function

--- src/NPSModal.js
+++ src/NPSModal.js
@@ -1,14 +1,93 @@
 import React from 'react'
+import _ from 'lodash'
+
+const PROMOTER_FOLLOW_UP = ':)';
+const NEUTRAL_FOLLOW_UP  = ':|';
+const DETRACTOR_FOLLOW_UP  = ':(';
+
+const MIN_SCORE = 0;
+const MAX_SCORE = 10;
+const SCORES = _.range(MIN_SCORE, MAX_SCORE+1);
 
 class NPSModal extends React.Component {
 
     static propTypes = {
-        mainQuestion: React.PropTypes.string.isRequired
+        mainQuestion: React.PropTypes.string.isRequired,
+        onClose: React.PropTypes.func.isRequired,
+        applicationEndpoint: React.PropTypes.string
     }
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            showFollowUp: false,
+            score: null,
+            comment: null
+        };
     }
+
+    render() {
+        return (
+            <div>
+                { this.state.showFollowUp ? this.renderFollowUpForm() : this.renderMainQuestion() }
+            </div>
+        );
+    }
+
+    renderMainQuestion = () => {
+        return (
+            <div>
+                <div>{this.props.mainQuestion}</div>
+                <div>
+                    {_.map(SCORES, score => {
+                        return (
+                            <div key={score} onClick={() => this.onScoreClick(score)}> {score} </div>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    };
+
+    onScoreClick = (score) => {
+        // TODO: Fire event to snoopy
+        // TODO: Send data to endpoint
+
+        this.setState({showFollowUp: true, score});
+    };
+
+    renderFollowUpForm = () => {
+        const followUpQuestion = this.deriveFollowUpQuestion();
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <label> {followUpQuestion} </label>
+                <input type="text" value={this.state.comment} onChange={this.handleCommentChange()} />
+                <input type="submit" value="Submit" />
+            </form>
+
+        );
+    };
+
+    deriveFollowUpQuestion = () => {
+        if (this.state.score < 7) {
+            return DETRACTOR_FOLLOW_UP;
+        } else if (this.state.score > 8) {
+            return PROMOTER_FOLLOW_UP;
+        } else {
+            return NEUTRAL_FOLLOW_UP;
+        }
+    };
+
+    handleCommentChange = (event) => {
+        this.setState({comment: event.target.value});
+    };
+
+    onSubmit = () => {
+        // TODO: Fire event to snoopy
+        // TODO: Send data to endpoint
+    };
 
 }
 
--- src/NPSScale.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-class NPSScale extends React.Component {
-
-}
-
-export default NPSScale
